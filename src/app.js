f727e8648185a639d14af306fcb41247d3aafecb
David Campbell <dcampbell092@gmail.com>
2020-02-07 15:02:19 +0000

    api files added

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/main.scss'
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>React Starter Pack</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
