cd9ba7e6d20f1d4135eb4259ff9044074be75f15
Clare Robertson <clare.robertson@gaincredit.com>
2020-02-07 14:47:35 +0000

    create models and part of seed

---
+++ models/db/seeds.js
@@ -0,0 +1,16 @@
+{
+  name: "The Pensioner",
+  image: "",
+  city: "London",
+  streetName: "28 Bazely",
+  postcode: "E14 0ES",
+  phone: ,
+  website: "",
+  description: "Nice cosy pub",
+  maxTeamSize: 6,
+  quizDay: "Tuesday",
+  quizTime: "7:30pm",
+  starRating: [2, 3],
+  averagePintCost: "£4.50",
+  
+}
---
+++ models/pub.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const reviewSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+},
+{ 
+  timestamps: true
+}
+)
+
+const pubSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  city: { type: String, required: true },
+  streetName: { type: String, required: true },
+  postcode: { type: String, required: true, unique: true },
+  phone: { type: String },
+  website: { type: String },
+  description: { type: String, maxlength: 500 },
+  maxTeamSize: { type: Number, required: true },
+  quizDay: { type: String, required: true },
+  quizTime: { type: String, required: true },
+  starRating: [2, 3],
+  averagePintCost: { type: String, required: true },
+  reviews: [ reviewSchema ],
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Pub', pubSchema)
