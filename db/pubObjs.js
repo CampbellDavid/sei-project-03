fff1a59c2bb69022b8278eaa441190f11464e4b9
cara <xw33@outlook.com>
2020-02-08 07:07:49 +0000

    add seeds file

---
+++ db/pubObjs.js
@@ -0,0 +1,35 @@
+let createdUsers
+[
+  {
+    name: 'Abbey Bar',
+    image: 'http://www.pubquizzers.com/images/pubs/abbey-bar_560.jpg',
+    city: 'London',
+    streetName: 'Tower Hill',
+    postcode: 'EC3N 1DD',
+    phone: '020 7488 1918',
+    website: 'abbey-bar.co.uk',
+    description: 'Join us at Abbey for our Legendary pub quiz. The winning team will walk away with a £250 bar tab to use at Abbey, as well as a trophy to keep until the next quiz. Entry is FREE and complimentary nibbles are provided throughout the evening.',
+    maxTeamSize: 8,
+    quizDay: 'Tuesday',
+    quizTime: '18:30', // discover time format
+    starRating: [1, 2, 3, 4, 5, 5, 5, 5],
+    averagePintCost: '£6.50',
+    reviews: ['This was great', 'great service', 'quiz was awesome'],
+    user: createdUsers[0]
+  }, {
+    name: 'The Red Lion',
+    image: 'http://www.pubquizzers.com/images/pubs/abbey-bar_560.jpg',
+    city: 'London',
+    streetName: '48 Parliament St',//
+    postcode: 'SW1A 2NH',//
+    phone: '020 7930 5826',//
+    website: 'redlionwestminster.co.uk',//
+    description: 'Westminster pub with elaborate ceiling, politicians\' portraits and illustrious former clientele.',
+    maxTeamSize: 6,
+    quizDay: 'Friday',
+    quizTime: '19:00', // discover time format
+    starRating: [1, 2, 1, 4, 1, 2, 5, 1],
+    averagePintCost: '£5.00',
+    reviews: ['Good service', 'poor service', 'quiz was hard'],
+    user: createdUsers[1]
+  }]
--- db/seeds.js
+++ db/seeds.js
@@ -1,16 +1,35 @@
-{
-  name: "The Pensioner",
-  image: "",
-  city: "London",
-  streetName: "28 Bazely",
-  postcode: "E14 0ES",
-  phone: ,
-  website: "",
-  description: "Nice cosy pub",
-  maxTeamSize: 6,
-  quizDay: "Tuesday",
-  quizTime: "7:30pm",
-  starRating: [2, 3],
-  averagePintCost: "£4.50",
-  
-}
+const mongoose = require('mongoose')
+const { dbURI } = require('../config/environment')
+const Pub = require('../models/pub')
+const User = require('..models/user')
+const PubObjs = require('./pubObjs')
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+  if (err) return console.log(err)
+  db.dropDatabase()
+    .then(() => {
+      return User.create([
+        {
+          username: '1',
+          email: '1@email.com',
+          password: '1',
+          passwordConfirmation: '1'
+        }, 
+        {
+          username: '2',
+          email: '2@email.com',
+          password: '2',
+          passwordConfirmation: '2'
+        }
+      ])
+    })
+    .then(createdUsers => {
+      console.log(`${'🍺'.repeat(createdUsers.length)} users created`)
+      return Pub.create(PubObjs)
+    })
+    .then(createdPubs => console.log(`${'🍺'.repeat(createdPubs.length)} pubs created `))
+    .catch(err => console.log(err))
+    .finally(() => mongoose.connection.close())
+})
+
+
