c9bc0821be9431359913c5e2528c78bed3a65899
David Campbell <dcampbell092@gmail.com>
2020-02-09 11:48:23 +0000

    added controllers

---
+++ controllers/events.js
@@ -0,0 +1,57 @@
+const Event = require('../models/event')
+
+function index(req, res) {
+  Event
+    .find()
+    .populate('user')
+    .then(foundEvents => res.status(200).json(foundEvents))
+    .catch(err => res.json(err))
+}
+
+function create(req, res, next) {
+  req.body.user = req.currentUser
+  Event
+    .create(req.body)
+    .then(createdEvent => res.status(201).json(createdEvent))
+    .catch(next)
+}
+
+function show(req, res, next) {
+  Event
+    .findById(req.params.id)
+    .populate('user')
+    .then(event => { 
+      if (!event) throw new Error('Not Found')
+      res.status(200).json(event)
+    })
+    .catch(next)
+}
+
+function update(req, res, next) {
+  Event
+    .findById(req.params.id)
+    .then(event => {
+      if (!event) throw new Error('Not Found')
+      if (!event.user.equals(req.currentUser._id)) return res.status(401).json({ message: 'Unauthorised' })
+      Object.assign(event, req.body) 
+      return event.save()  
+    })
+    .then(updatedEvent => res.status(202).json(updatedEvent)) 
+    .catch(next)
+}
+
+function destroy(req, res) {
+  Event
+    .findById(req.params.id)
+    .then(event => {
+      if (!event) return res.status(404).json({ message: 'Not Found ' })
+      if (!event.user.equals(req.currentUser._id)) {
+        res.status(401).json({ message: 'Unauthorised' })
+      } else {
+        event.remove().then(() => res.sendStatus(204))
+      }
+    })
+    .catch(err => res.json(err))
+}
+
+module.exports = { index, create, show, update, destroy }
