cc6d335732849f08e5862fce33a3a921a479dac8
cara <xw33@outlook.com>
2020-02-09 11:41:54 +0000

    plan frontend components-see comments at the bottom of app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import './styles/main.scss'
 class App extends React.Component {
   render() {
     return (
-      <h1>React Starter Pack</h1>
+      <h1>Inquizitour🍺🍺🍺🍺</h1>
     )
   }
 }
@@ -14,3 +14,14 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
+
+
+//! PAGES
+//* HOME
+//TODO COMPONENTS: NAVBAR
+
+//* PUB INDEX
+//TODO COMPONENTS: SEARCHBAR, NAVBAR, MAP, FEATURED PUBS(IMPORT FROM PUBCARD COMPONENT), CREATE NEW PUBS BUTTON
+
+//* EVENT INDEX
+//TODO COMPONENTS: SEARCHBAR, NAVBAR, MAP, FEATURED PUBS(IMPORT FROM PUBCARD COMPONENT), CREATE NEW EVENTS BUTTON
