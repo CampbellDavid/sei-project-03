2cfaed9d9a14b365190ff05e2aca0d1f4607d5d3
Adriancusniriuc <cusniriuc.adrian05@gmail.com>
2020-02-09 11:48:20 +0000

    create pub controller

--- controllers/pubs.js
+++ controllers/pubs.js
@@ -9,4 +9,91 @@
 
 const Pub = require('../models/pub')
 
-module.exports = { index, create, show, update, destroy, createReview, deleteReview }
+function index(req, res) {
+  Pub
+    .find()
+    .populate('user')
+    .then(foundPubs => res.status(200).json(foundPubs))
+    .catch(err => res.json(err))
+}
+
+function create(req, res, next) {
+  req.body.user = req.currentUser
+  Pub
+    .create(req.body)
+    .then(createdPub => res.status(201).json(createdPub))
+    .catch(next)
+}
+
+function show(req, res, next) {
+  Pub
+    .findById(req.params.id)
+    .populate('user')
+    .then(pub => { 
+      if (!pub) throw new Error('Not Found')
+      res.status(200).json(pub)
+    })
+    .catch(next)
+}
+
+function update(req, res, next) {
+  Pub
+    .findById(req.params.id)
+    .then(pub => {
+      if (!pub) throw new Error('Not Found')
+      if (!pub.user.equals(req.currentUser._id)) return res.status(401).json({ message: 'Unauthorised' })
+      Object.assign(pub, req.body) 
+      return pub.save()  
+    })
+    .then(updatedPub => res.status(202).json(updatedPub)) 
+    .catch(next)
+}
+
+function destroy(req, res) {
+  Pub
+    .findById(req.params.id)
+    .then(pub => {
+      if (!pub) return res.status(404).json({ message: 'Not Found ' })
+      if (!pub.user.equals(req.currentUser._id)) {
+        res.status(401).json({ message: 'Unauthorised' })
+      } else {
+        pub.remove().then(() => res.sendStatus(204))
+      }
+    })
+    .catch(err => res.json(err))
+}
+
+// * POST /pubs/:id/reviews 
+function reviewCreate(req, res) {
+  req.body.user = req.currentUser
+  Pub
+    .findById(req.params.id)
+    .then(pub => {
+      if (!pub) return res.status(404).json({ message: 'Not Found ' })
+      pub.reviews.push(req.body)
+      return pub.save()
+    })
+    .then(pub => res.status(201).json(pub))
+    .catch(err => res.json(err))
+}
+
+// * DELETE /pubs/:id/reviews/:reviewId
+function reviewDelete(req, res) {
+  Pub
+    .findById(req.params.id)
+    .then(pub => { 
+      if (!pub) return res.status(404).json({ message: 'Not Found ' })
+      const review = pub.reviews.id(req.params.reviewId)
+      if (!review) return res.status(404).json({ message: 'Not Found ' })
+      if (!review.user.equals(req.currentUser._id)) return res.status(401).json({ message: 'Unauthorised' })
+      review.remove()
+      return pub.save()
+    })
+    .then(pub => res.status(202).json(pub))
+    .catch(err => res.json(err))
+}
+
+
+
+
+module.exports = { index, create, show, update, destroy, reviewCreate, reviewDelete }
--- controllers/teams.js
+++ controllers/teams.js
@@ -7,4 +7,13 @@
 
 const Pub = require('../models/pub')
 
+function create(req, res, next) {
+  req.body.user = req.currentUser
+  Team
+    .create(req.body)
+    .then(createdPub => res.status(201).json(createdPub))
+    .catch(next)
+}
+
+
 module.exports = { create, update, destroy }
