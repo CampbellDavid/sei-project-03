a878db1d19acd968748589ee2a6cbf9844acd135
cara <xw33@outlook.com>
2020-02-09 12:41:40 +0000

    add app.js to get the basic frontend running

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/main.scss'
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
+
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Inquizitour🍺🍺🍺🍺</h1>
+      <BrowserRouter>
+        <main>
+          <h1>Inquizitour🍺🍺🍺🍺</h1>
+          {/* <nav className='navbar'>
+            <Link to='/'>Home</Link>
+            <Link to='/pubs'>Find a pub!</Link>
+            <Link to='/events'>Find a quiz!</Link>
+            <Link to='/register'>Register</Link>
+          </nav>
+          <Switch>
+            <Route exact path='/' component={Home}></Route>
+            <Route path='/pubs' component={PubIndex}></Route>
+            <Route path='/events' component={EventIndex}></Route>
+            <Route path='/register' component={Register}></Route>
+          </Switch> */}
+        </main>
+      </BrowserRouter>
     )
   }
 }
