803e3777536d032dcdd102f82cde22552a8858a2
Adriancusniriuc <cusniriuc.adrian05@gmail.com>
2020-02-10 09:05:45 +0000

    pub index in progress

--- src/components/pubs/PubIndex.js
+++ src/components/pubs/PubIndex.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import axios from 'axios'
+import 'mapbox-gl/mapbox-gl.css'  
+import 'react-map-gl-geocoder/mapbox-gl-geocoder.css'
 import MapGL, {  Marker, Popup } from 'react-map-gl'
 import Geocoder from 'react-map-gl-geocoder'
 
@@ -9,12 +11,45 @@ import PubCard from './PubCard'
 
 export default class PubIndex extends React.Component {
 state = {
-  pubs: []
+  pubs: [],
+  viewport: {
+    latitude: 51.5074,
+    longitude: 0.1278,
+    zoom: 7
+  }
 }
 
-aysnc componentDidMount() {
-  
+async componentDidMount() {
+  try {
+    const res = await axios.get(`/api/pubs/${pubId}`)
+    console.log(res.data)
+    this.setState({ pubs: res.data })
+
+  } catch (error) {
+    console.log(error)
+  }
 }
+
+mapRef = React.createRef()
+  handleViewportChange = (viewport) => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    })
+  }
+
+  handleGeocoderViewportChange = (viewport) => {
+    console.log(viewport)
+    const geocoderDefaultOverrides = { transitionDuration: 3000 }
+    this.setState({ viewport: { latitude: viewport.latitude, longitude: viewport.longitude, zoom: viewport.zoom } })
+    // this.apiCall()
+    console.log(this.state.viewport)
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    })
+  }
+
+
 
 render() {
   return (
