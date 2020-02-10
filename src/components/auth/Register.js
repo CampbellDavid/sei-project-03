22c9458768ddfb3446bf101a937c6412dabb728b
Clare Robertson <clare.robertson@gaincredit.com>
2020-02-10 09:30:21 +0000

    add class component and form fields to Register

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,78 @@
 import React from 'react'
+import axios from 'axios'
 
-const Register = () => (
-  <h1>Register page working</h1>
-)
+export default class Register extends React.Component {
+state = {
+  fields: {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+}
 
-export default Register
+  handleChange = e => {
+    const fields = { ...this.state.fields, [e.target.name]: e.target.value } 
+    this.setState({ fields })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      await axios.post('/api/register', this.state.fields)
+      this.props.history.push('/login')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    console.log(this.state.fields)
+    return (
+      <div>
+        <form 
+          onSubmit={this.handleSubmit}
+        >
+          <h2>Register to Comment</h2>
+          <div className="form-div">
+            <label>Username:</label>
+            <input
+              onChange={this.handleChange} 
+              placeholder="username"
+              name="username" />
+          </div>
+          <div className="form-div">
+            <label>Email:</label>
+            <input
+              onChange={this.handleChange} 
+              type="email" 
+              placeholder="email"
+              name="email" />
+          </div>
+          <div className="form-div">
+            <label>Password:</label>
+            <input
+              onChange={this.handleChange} 
+              type="password" 
+              placeholder="password"
+              name="password" />
+          </div>
+          <div className="form-div">
+            <label>Confirm Password:</label>
+            <input
+              onChange={this.handleChange} 
+              type="password" 
+              placeholder="confirm password"
+              name="confirmPassword" />
+          </div>
+          <div className="button-div">
+            <button
+              className="button" 
+              type="submit">
+              Register</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
