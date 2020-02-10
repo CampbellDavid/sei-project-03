7fee8b9741c6ba90dd83714fe79e8df610951a93
Clare Robertson <clare.robertson@gaincredit.com>
2020-02-10 10:02:16 +0000

    add basic login form to Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,51 @@
 import React from 'react'
 
-const Login = () => (
-  <h1>Login page working</h1>
-)
+export default class Login extends React.Component {
+state = {
+  fields: {
+    username: '',
+    password: ''
+  }
+}
 
-export default Login
+handleChange = e => {
+  const fields = { ...this.state.fields, [e.target.name]: e.target.value }
+  this.setState({ fields })
+}
+
+handleClick = e => {
+  e.preventDefault
+}
+
+render() {
+  console.log(this.state.fields)
+  return (
+    <div>
+      <form onClick={this.handleClick}>
+        <h2>Login Here</h2>
+        <div className="form-div">
+          <label>Username:</label>
+          <input
+            placeholder="username"
+            name="username" 
+            onChange={this.handleChange}/>
+        </div>
+        <div className="form-div">
+          <label>Password:</label>
+          <input
+            type="password" 
+            placeholder="password"
+            name="password"
+            onChange={this.handleChange} />
+        </div>
+        <div className="button-div">
+          <button
+            className="button" 
+            type="submit">
+            Register</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+}
