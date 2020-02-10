22efb3ecd9d5913125bdb41aee9076bac580aa55
cara <xw33@outlook.com>
2020-02-10 23:11:19 +0000

    fix merge conflict

--- src/components/pubs/PubIndex.js
+++ src/components/pubs/PubIndex.js
@@ -30,26 +30,6 @@ async componentDidMount() {
   }
 }
 
-<<<<<<< HEAD
-mapRef = React.createRef()
-  handleViewportChange = (viewport) => {
-    this.setState({
-      viewport: { ...this.state.viewport, ...viewport }
-    })
-  }
-
-  handleGeocoderViewportChange = (viewport) => {
-    console.log(viewport)
-    const geocoderDefaultOverrides = { transitionDuration: 3000 }
-    this.setState({ viewport: { latitude: viewport.latitude, longitude: viewport.longitude, zoom: viewport.zoom } })
-    // this.apiCall()
-    console.log(this.state.viewport)
-    return this.handleViewportChange({
-      ...viewport,
-      ...geocoderDefaultOverrides
-    })
-  }
-=======
 render() {
   console.log(1)
   if (!this.state.postcodes) return null
@@ -82,7 +62,6 @@ render() {
           </div>
         </Marker>
       } )}
->>>>>>> 4c35fe0e1a65cee91db82e943fc67d310196eac6
 
 
 
