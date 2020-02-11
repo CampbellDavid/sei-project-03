50dfe0b368eb3f73b4fae631062c83edaba38639
cara <xw33@outlook.com>
2020-02-11 14:43:41 +0000

    refactor refactor +.+

--- src/components/pubs/PubIndex.js
+++ src/components/pubs/PubIndex.js
@@ -2,64 +2,98 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import React from 'react'
 import axios from 'axios'
-// import MapGL, {  Marker } from 'react-map-gl'
-// import Geocoder from 'react-map-gl-geocoder'
-import Map from '../common/Map'
-// const mapboxToken = process.env.MAPBOX_ACCESS_TOKEN
-// import PubCard from './PubCard'
+import Geocoder from 'react-map-gl-geocoder'
+import MapGL, { Marker } from 'react-map-gl'
+
+
+
+const mapboxToken = process.env.MAPBOX_ACCESS_TOKEN
+
 export default class PubIndex extends React.Component {
-state = {
-  postcodes: [],
-  pubs: [],
-  viewport: {
-    latitude: 51.5074,
-    longitude: 0.1278,
-    zoom: 9
+  state = {
+    postcodes: [],
+    pubs: [],
+    viewport: {
+      latitude: 51.5074,
+      longitude: 0.1278,
+      zoom: 9
+    }
   }
-  // showInfo: false
-}
-// mapRef = React.createRef()
-handleViewportChange = (viewport) => {
-  this.setState({
-    viewport: { ...this.state.viewport, ...viewport }
-  })
-}
-handleGeocoderViewportChange = (viewport) => {
-  // console.log(viewport)
-  const geocoderDefaultOverrides = { transitionDuration: 1000 }
-  this.setState({ viewport: { latitude: viewport.latitude, longitude: viewport.longitude, zoom: viewport.zoom } })
-  // this.apiCall()
-  // console.log(this.state.viewport)
-  return this.handleViewportChange({
-    ...viewport,
-    ...geocoderDefaultOverrides
-  })
-}
-async componentDidMount() {
-  console.log('hello')
-  try {
-    const res = await axios.get('/api/pubs')
-    this.setState({ pubs: res.data })
-    this.getPostcodes()
-  } catch (error) {
-    console.log(error)
+  mapboxToken = process.env.MAPBOX_ACCESS_TOKEN
+  mapRef = React.createRef()
+  handleViewportChange = (viewport) => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    })
+    console.log(this.state.viewport)
   }
-}
-async getPostcodes () {
-  const postcodes = this.state.pubs.map( pub => pub.postcode)
-  const res = await axios.post('https://cors-anywhere.herokuapp.com/api.postcodes.io/postcodes', { postcodes })
-  console.log(res.data.result)
-  // console.log(postcodes.result)
-  this.setState({ postcodes: res.data.result })
-}
-render() {
-  console.log(1)
-  if (!this.state.postcodes) return null
-  console.log(this.state.postcodes.map(postcode => postcode.result.longitude))
-  return (
-    <>
-      <Map />
-    </>
-  )
-}
+
+  handleGeocoderViewportChange = (viewport) => {
+
+    const geocoderDefaultOverrides = { transitionDuration: 1000 }
+    this.setState({ ...viewport })
+    this.handleViewportChange({ ...viewport, ...geocoderDefaultOverrides })
+    
+  
+  }
+  async getPostcodes () {
+    const postcodes = this.state.pubs.map( pub => pub.postcode)
+    const res = await axios.get('https://cors-anywhere.herokuapp.com/api.postcodes.io/postcodes', { postcodes })
+    console.log(res)
+    
+    this.setState({ postcodes: res.data.result })
+  }
+
+  async componentDidMount() {
+    console.log('mounting')
+    try {
+      const res = await axios.get('/api/pubs')
+      this.setState({ pubs: res.data })
+      this.getPostcodes()
+      console.log(this.getPostcodes(), res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  render() {
+    console.log('rendering')
+    if (!this.state.postcodes) return null
+
+    return (
+
+
+
+      <MapGL
+        mapboxApiAccessToken={mapboxToken}
+        ref={this.mapRef}
+        {...this.state.viewport}
+        height={'50vh'}
+        width={'50vw'}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        // zoom={10}
+        // latitude= {51.5074}
+        // longitude= {0.1278}
+        onViewportChange={this.handleViewportChange}>
+        <Geocoder
+          mapRef={this.mapRef}
+          onViewportChange={this.handleGeocoderViewportChange}
+          mapboxApiAccessToken={mapboxToken}
+        />
+
+        {this.state.postcodes.map((postcode, index) => {
+          return <Marker
+            key={index.toString()}
+            latitude={postcode.result.latitude}
+            longitude={postcode.result.longitude} >
+            <div className="marker">
+            </div>
+          </Marker>
+        } )}
+      </MapGL>
+
+
+      
+    )
+  }
 }
