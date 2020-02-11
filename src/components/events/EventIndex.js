fce1d359f166e0896d181536656b4bd1092cd91a
Clare Robertson <clare.robertson@gaincredit.com>
2020-02-11 17:15:48 +0000

    update EventIndex & EventCard

--- src/components/events/EventIndex.js
+++ src/components/events/EventIndex.js
@@ -5,28 +5,39 @@ import EventCard from './EventCard'
 
 export default class EventIndex extends React.Component {
   state = {
-    events: null
+    pubs: null
   }
+
+  // async componentDidMount() {
+  //   try {
+  //     const res = await axios.get('/api/events')
+  //     // console.log(res.data)
+  //     this.setState({ events: res.data })
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   async componentDidMount() {
     try {
-      const res = await axios.get('/api/events')
-      // console.log(res.data)
-      this.setState({ events: res.data })
+      const res = await axios.get('/api/pubs')
+      console.log('res data', res.data)
+      this.setState({ pubs: res.data })
     } catch (err) {
       console.log(err)
     }
   }
 
   render() {
-    if (!this.state.events) return null
+    console.log('pubs', this.state.pubs)
+    if (!this.state.pubs) return null
     // const eventArr = this.state.events
     return (
       <section className="event-index-section">
         <h1>Find a pub quiz near you!</h1>
         <div className="container">
-          {this.state.events.map(event => (
-            <EventCard key={event._id} {...event} />
+          {this.state.pubs.map(pub => (
+            <EventCard key={pub._id} {...pub} />
           ))}
         </div>
       </section>
