8db5b7cd6cf1f7ceeba5b1b9b7a626e656d7e2d0
cara <xw33@outlook.com>
2020-02-12 16:17:20 +0000

    event index mapm component

--- src/components/events/EventIndex.js
+++ src/components/events/EventIndex.js
@@ -1,31 +1,69 @@
 import React from 'react'
 import axios from 'axios'
-import EventCard from './EventCard'
+// import EventCard from './EventCard'
+import PubMap from '../common/PubMap'
 
 
 export default class EventIndex extends React.Component {
   state = {
-    pubs: null
+    postcodes: null,
+    pubs: null,
+    viewport: {
+      latitude: 51.5074,
+      longitude: 0.1278,
+      zoom: 9
+    },
+    showPopup: true
   }
 
-  // async componentDidMount() {
-  //   try {
-  //     const res = await axios.get('/api/events')
-  //     // console.log(res.data)
-  //     this.setState({ events: res.data })
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
+  mapboxToken = process.env.MAPBOX_ACCESS_TOKEN
+  mapRef = React.createRef()
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    })
+  }
+
+  handleGeocoderViewportChange = viewport => {
+
+    const geocoderDefaultOverrides = { transitionDuration: 1000 }
+    this.setState({
+      viewport: {
+        latitude: viewport.latitude,
+        longitude: viewport.longitude,
+        zoom: viewport.zoom
+      }
+    })
+
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    })
+  };
 
   async componentDidMount() {
     try {
       const res = await axios.get('/api/pubs')
-      console.log('res data', res.data)
       this.setState({ pubs: res.data })
-    } catch (err) {
-      console.log(err)
+      this.getPostcodes()
+    } catch (error) {
+      console.log(error)
     }
+  }
+
+  async getPostcodes() {
+    const postcodes = this.state.pubs.map(pub => {
+      return pub.postcode
+    })
+
+    const res = await axios.post(
+      'https://cors-anywhere.herokuapp.com/api.postcodes.io/postcodes',
+      { postcodes }
+    )
+    console.log(res.data.result)
+
+    this.setState({ postcodes: res.data.result })
   }
 
   render() {
@@ -34,12 +72,22 @@ export default class EventIndex extends React.Component {
     // const eventArr = this.state.events
     return (
       <section className="event-index-section">
+
         <h1>Find a pub quiz near you!</h1>
-        <div className="container">
+        {/* <div className="container">
           {this.state.pubs.map(pub => (
             <EventCard key={pub._id} {...pub} />
           ))}
-        </div>
+        </div> */}
+        <PubMap 
+          viewport={this.state.viewport} 
+          handleGeocoderViewportChange={this.handleGeocoderViewportChange}
+          handleViewportChange={this.handleViewportChange} 
+          mapboxToken={this.mapboxToken}
+          mapRef={this.mapRef} 
+          postcodes={this.state.postcodes}
+          pubs={this.state.pubs} 
+        />
       </section>
     )
   }
