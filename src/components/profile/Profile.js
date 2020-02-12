2ddc0b1d32c6a8d7bc84268c223de4901d593cc3
cara <xw33@outlook.com>
2020-02-12 22:12:46 +0000

    start profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,39 @@
 import React from 'react'
 
-const Profile = () => (
-  <h1>This is the user profile page after login</h1>
-)
+
+class Profile extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
 
 export default Profile
+
+
+//TODO
+//* after login, redirect to user profile page, show the following information
+
+//* embed team card (if currentUser belongs to certain teams)
+
+//* team card should contain links to user profiles (non-secure route)
+//* public user profiles have a direct message button => DirectMessage component
+
+
+
+
+
+
+
+//username: { type: String, required: true, unique: true },
+// email: { type: String, required: true, unique: true },
+// password: { type: String, required: true },
+// favouriteDrinks: [{ type: String }],
+// personalityType: { type: String },
+// bio: { type: String },
+// age: { type: Number },
+// gender: { type: String },
+// quizStrengths: [{ type: String }],
+// profileImage: { type: String }
+
