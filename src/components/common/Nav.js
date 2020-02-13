272305690c4ee30c9b2c0e250983dee6e740f841
Clare Robertson <clare.robertson@gaincredit.com>
2020-02-13 05:54:26 +0000

    style edits, remove throwaway nav links

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -4,12 +4,8 @@ import { Link } from 'react-router-dom'
 const Nav = () => (
   <nav className="navbar">
     <Link className="nav-item" to="/">Home</Link>
-    {/* //! CR: below to be deleted later */}
-    <Link className="nav-item" to="/pubs/new">New Pub</Link>
     <Link className="nav-item" to="/pubs">Pubs</Link>
     <Link className="nav-item" to="/events">Events</Link>
-    {/* //! CR: below to be deleted later */}
-    <Link className="nav-item" to="/events/new">New Event</Link>
     <Link className="nav-item" to="/login">Login</Link>
     <Link className="nav-item" to="/register">Register</Link>
     {/* //! XW: test link; will delete later */}
--- src/components/pubs/PubCard.js
+++ src/components/pubs/PubCard.js
@@ -2,23 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PubCard = ({ _id, name, image, starRating }) => (
-  <section className="column">
+  <section className="card-container">
     <Link to={`/pubs/${_id}`}>
-
-      <div className="pub-index-card">
-
-        <h2 className="header">{name}</h2>
-        
-        <p>Rating: {starRating}</p>
-
-        <div className="card-container">
+      <div className="card">
+        <div className="colour-header">
           <figure className="image">
             <img src={image} alt={name} />
           </figure>
         </div>
-
+        <div className="card-info">
+          <h3>{name}</h3>
+          <p>Rating: {starRating}</p>
+        </div>
       </div>
-
     </Link>
   </section>
 )
