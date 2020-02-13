923f7a821c1e61fa15e8de8057d49fe46e8d961b
cara <xw33@outlook.com>
2020-02-13 16:36:10 +0000

    add link to user profile

--- src/components/teams/TeamCard.js
+++ src/components/teams/TeamCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Authorization from '../../../lib/authorization'
+import { Link } from 'react-router-dom'
 
 const TeamCard = ({ members, captain, event, teamName }) => (
   <>
@@ -7,7 +8,7 @@ const TeamCard = ({ members, captain, event, teamName }) => (
     <h2>{event}</h2>
     <h3>Captain: {captain.username}</h3>
     <h3>Members: {members.map((member, i) => {
-      return <li key={i}>{member.username}</li>
+      return <li key={i}><Link to={`/profiles/${member._id}`}>{member.username}</Link></li>
     })}</h3>
 
     {Authorization.isAuthenticated() ?
