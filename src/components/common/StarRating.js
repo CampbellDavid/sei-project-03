ec843bb0d1d331e28ef72e2b3130449cae635600
David Campbell <dcampbell092@gmail.com>
2020-02-14 13:17:58 +0000

    logout btn chg color

--- src/components/common/StarRating.js
+++ src/components/common/StarRating.js
@@ -33,19 +33,3 @@ export default class App extends Component {
     )
   }
 }
-
-// import React, { Component } from 'react'
-
-// import BeautyStars from 'beauty-stars'
-
-// export default class App extends Component {
-//   state = { value: 0 };
-//   render() {
-//     return (
-//       <BeautyStars
-//         value={this.state.value}
-//         onChange={value => this.setState({ value })}
-//       />
-//     )
-//   }
-// }
