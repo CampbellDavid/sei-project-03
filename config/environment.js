2562888c02df303ae30f084110f268445e9af440
David Campbell <dcampbell092@gmail.com>
2020-03-05 13:14:16 +0000

    mapbox deploy test

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,8 @@
 const env = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/pub-quiz-sei-project-${env}`
+const dbURI =
+	process.env.MONGODB_URI || `mongodb://localhost/pub-quiz-sei-project-${env}`
 const secret = process.env.SECRET || 'I think, therefore I am'
-
 
 console.log({ port, dbURI, secret })
 
